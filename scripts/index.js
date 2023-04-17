7e537a31f8234210dfd72053091ece71082288e5
孤之界 <f20001012@gmail.com>
2023-04-17 15:30:55 +0800

    add: 產生訊息

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
+const { throwInfo } = require('./lib/helpers/utils')
+
 require('./lib/filters')(hexo)
 require('./lib/generators')(hexo)
 require('./lib/injector')(hexo)
+
+throwInfo(
+  'Aurora Generator',
+  `Start Generator`
+)
